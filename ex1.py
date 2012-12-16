86e9d68ddb0e99a1b8044c8d413f2eb47fb95caf
Eric Little <eric.j.little@gmail.com>
2012-12-15 21:05:07 -0500

    E1 - Fixed Types

--- ex1.py
+++ ex1.py
@@ -1,7 +1,7 @@
 print "Hello World!"
 print "Hello Again"
-print "I like tryping this."
-print "this is fun."
+print "I like typing this."
+print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
