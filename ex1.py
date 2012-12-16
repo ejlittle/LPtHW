38eb36b0f3b34f20d8c7589858a7da62e13640b1
Eric Little <eric.j.little@gmail.com>
2012-12-15 21:09:11 -0500

    Completed Extra Credit

--- ex1.py
+++ ex1.py
@@ -1,7 +1,8 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
 print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+#print 'I touched it. What are you going to do about it?'
