32a39c5dc0b2b0bdfbde3c2695749175f79ba0e1
Eric Little <eric.j.little@gmail.com>
2012-12-18 10:57:17 -0500

    Completed Extra Credit

--- ex5.py
+++ ex5.py
@@ -1,17 +1,19 @@
-my_name = 'Eric J. Little'
-my_age = 25
-my_height = 71
-my_weight = 195
-my_eyes = 'Green'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Eric J. Little'
+age = 25
+height = 71
+weight = 195
+eyes = 'Green'
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
+
+print "If you aren't from America, I am %f centimeters tall and %f kilos heavy." % (height * 2.54, weight * 0.453592)
 
